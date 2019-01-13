1b29c334c5eca41c6b4444bca98db1989ec032ac
Thomas Zetty <thomaszetty@gmail.com>
2019-01-13 08:43:16 -0600

    midi util boilerplate

---
+++ mutil.py
@@ -0,0 +1,12 @@
+import sys
+import random
+import dlib
+
+# https://docs.python.org/3.3/tutorial/inputoutput.html#reading-and-writing-files
+
+def main(args):
+    print('midi utility')
+    print(args)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
