57130bb53db7d8aab467a30634ce0b1961aef60f
Thomas Zetty <thomaszetty@gmail.com>
2019-02-01 19:30:29 -0600

    add os.path and DIRNAME_ONLY option

--- pwalk.py
+++ pwalk.py
@@ -3,6 +3,8 @@ import random
 import os
 import dlib
 
+DIRNAME_ONLY = 'DIRNAME_ONLY'
+
 def get_root_value(arg):
     if arg == '@':
         return dlib.read_file('roots.txt').strip()
@@ -24,6 +26,7 @@ def is_match(filename, exts):
 def main(args):
     if len(args) == 0:
         print('usage: pwalk <root> [extension]+')
+        print('    pwalk @shake nrkt DIRNAME_ONLY')
         return 1
 
     root = get_root_value(args[0])
@@ -31,13 +34,21 @@ def main(args):
     exts = args[1:]
     assert len(exts) > 0
 
+    dirname_only = DIRNAME_ONLY in exts
+
     for dirName, subdirList, fileList in os.walk(root):
+        print_dir = False
         for filename in fileList:
             if is_match(filename, exts):
-                fullname = dirName + '/' + filename
-                if dirName.endswith('/'):
-                    fullname = dirName + filename
-                print('%s' % fullname)
+                if dirname_only:
+                    print_dir = True
+                else:
+                    fullname = os.path.join(dirName + '/' + filename)
+                    print('%s' % fullname)
+
+        if print_dir:
+            print('%s' % dirName)
+
     return 0
 
 if __name__ == '__main__':
